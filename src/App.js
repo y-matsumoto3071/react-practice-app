94b1a406571ffbd75afe62e6450bab1ecbc7f957
y-matsumoto3071 <y_matsumoto@nexus-nt.co.jp>
2024-02-10 19:46:22 +0900

    不要な記述の削除

--- src/App.js
+++ src/App.js
@@ -1,13 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Hello Pyoru!!</h2>
-      </header>
+      Hello Pyoru
     </div>
   );
 }
